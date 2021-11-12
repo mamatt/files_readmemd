c1786399f6011befadb7af350bdecad8ad633703
Matthieu Le Corre <matthieu.lecorre@univ-nantes.fr>
2021-11-12 14:57:41 +0100

    fix webpack config entry

--- webpack.js
+++ webpack.js
@@ -1,5 +1,9 @@
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+const path = require('path')
 
 webpackConfig.resolve['fallback']= {fs: false }
+
+webpackConfig.entry['main'] = path.join(__dirname, 'src', 'main.js'),
+webpackConfig.entry['config'] = path.join(__dirname, 'src', 'config.js')
 
 module.exports = webpackConfig
